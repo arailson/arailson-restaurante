a0f28cfa2a9b693d8d904591e19c402a8f6827b0
Arailson <arailson.vieira@gmail.com>
2022-10-27 11:51:46 -0300

    importando a logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from "../../assets/logo.svg"
 
 export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img  alt="logo do restaurante" src="../../assets/logo.svg"/>
+        <Logo />
       </nav>
     </main>
   );
