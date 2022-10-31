1d5b00195bd0faf3db8abdc23717125d9e84aace
Arailson <arailson.vieira@gmail.com>
2022-10-31 20:45:01 -0300

    Fazendo os filtros funcionarem

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,10 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
-  const [busca, setBusca] = useState("")
+  const [busca, setBusca] = useState("");
 
   return (
     <main>
@@ -12,13 +13,14 @@ export default function Cardapio() {
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa!
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa!</div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca}/>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros />
+        </div>
       </section>
     </main>
   );
