7217fa761c6d6e9802d927139b108f8738e0a366
Arailson <arailson.vieira@gmail.com>
2022-11-06 03:31:00 -0300

    Pratos recomendados

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Cardapio from "./pages/Cardapio";
 import "normalize.css";
+import Inicio from "pages/Inicio";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Cardapio />
+        <Inicio />
     </React.StrictMode>,
     document.getElementById("root")
 );
